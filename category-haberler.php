41b1209b400c631137270f93925f3f5bab77c58e
nuco <nesimiztrk@gmail.com>
2019-11-13 10:54:14 +0300

    tüm haberler düzenleme

--- category-haberler.php
+++ category-haberler.php
@@ -12,12 +12,12 @@ get_header(); ?>
 						<?php while ( have_posts() ) : the_post(); ?>
                             <div class="col-lg-4">
                                 <a title="<?php the_title_attribute(); ?>" href="<?php the_permalink() ?>" class="text-decoration-none">
-                                <div class="card mb-4" style="min-height: 400px">
+                                <div class="card mb-4" style="min-height: 350px">
                                     <img class="card-img-top" src="<?= ncoPostImage( $post->ID ); ?>" alt="image"
                                          style="width:100%; height: 220px">
                                     <div class="card-body">
                                         <h6 class="card-title text-dark font-weight-light">
-                                            <?= kisalt(get_the_title(),80); ?>
+                                            <?= kisalt(get_the_title(),100); ?>
                                         </h6>
                                     </div>
                                 </div>
