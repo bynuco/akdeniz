cd61757f745c50a800d4b1f2cff63bf75f6e0efd
nuco <nesimiztrk@gmail.com>
2019-11-13 11:48:06 +0300

    Menu TR Kaldırıldı

--- partials/newsArchivePartial.php
+++ partials/newsArchivePartial.php
@@ -188,82 +188,21 @@
             <div class="col-12 col-lg-12">
                 <div class="row">
                     <div class="col-lg-6">
-                        <a href="#">
+                        <a href="<?= site_url()."/basinda-bugun/"; ?>">
                             <img src="<?= base_url("assets/img/basindaBugun.png"); ?>" class="img-fluid img-thumbnail"
                                  alt="Basında Bügün">
                         </a>
                     </div>
                     <div class="col-lg-6">
-                        <a href="#">
+                        <a href="http://akdenizdenhaber.com/">
                             <img src="<?= base_url("assets/img/akdenizdenHaber.png"); ?>" class="img-fluid img-thumbnail"
                                  alt="Basında Bügün">
                         </a>
                     </div>
                 </div>
-                <!--
-				   <a href="#" class="btn  btn-block">Öğrenci <i class="fas fa-user float-right mt-1"></i></a>
-				   <a href="#" class="btn btn-block">Aday Öğrenci <i
-							   class="fas fa-graduation-cap float-right mt-1"></i></a>
-				   <a href="#" class="btn btn-block">Personel <i class="fas fa-users float-right mt-1"></i></a>
-				   <a href="#" class="btn btn-block">Bilimsel Araş. Projeleri Koor. Bir. <i
-							   class="fas fa-chart-bar float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Kütüphane <i class="fas fa-book-reader float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Hastane <i class="fas fa-hospital-alt float-right mt-1"></i></a>
-				   <a href="#" class="btn btn-block">Basın ve Halkla İlişkiler <i
-							   class="fas fa-newspaper float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Engelli <i class="fas fa-wheelchair float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Kalite <i class="fas fa-award float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Akdeniz Teknoloji Transfer Ofisi <i
-							   class="fas fa-caret-right float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Antalya Teknokent <i class="fas fa-tenge float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Uzaktan Eğitim <i
-							   class="fas fa-chalkboard-teacher float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Sürekli Eğitim <i class="fas fa-school float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Kampüste Yaşam <i class="fas fa-university float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Egit. ve Öğr. Mükemmellik A.U.M. <i
-							   class="fas fa-medal float-right mt-1"></i></a>
-				   <a href="#" class="btn btn-block">Yayınlar <i class="fas fa-book float-right mt-1"></i></a>
-				   <a href="http://ekampus.akdeniz.edu.tr/" class="btn  btn-block">E-Kampüs <i class="fab fa-etsy float-right mt-1"></i></a>
-				   <a href="#" class="btn  btn-block">Yemekhaneler <i class="fas fa-utensils float-right mt-1"></i></a>
-			   -->
+
             </div>
         </div>
     </div>
 
 </section>
-
-<!-- Archive -->
-<!--<section class="archive pt-5 pb-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 mb-4 mb-lg-2">
-                <div class="card text-center bg-card1">
-                    <div class="card-body p-0">
-                        <img alt="" class="img-fluid" src="<? /*= base_url( 'assets/img/Akdenizden10.jpg' ); */ ?>">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 mb-4 mb-lg-2">
-                <div class="card text-center bg-card2">
-                    <div class="card-body p-0">
-                        <img alt="" class="img-fluid" src="<? /*= base_url( 'assets/img/kurumsal-kimlik-alt.jpg' ); */ ?>">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 mb-4 mb-lg-2">
-                <div class="card text-center bg-card5">
-                    <div class="card-body p-0">
-                        <img alt="" class="img-fluid" src="<? /*= base_url( 'assets/img/Akdenizden10.jpg' ); */ ?>">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 mb-4 mb-lg-2">
-                <div class="card text-center bg-card6">
-                    <div class="card-body p-0">
-                        <img alt="" class="img-fluid" src="<? /*= base_url( 'assets/img/tanitim-katalogu.jpg' ); */ ?>">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>-->
