bbec20e8e9a61cef48d1db3ab71fbbef8603bda6
nuco <nesimiztrk@gmail.com>
2019-11-18 14:13:37 +0300

    404 Sayfası tasarımı eklendi

--- 404.php
+++ 404.php
@@ -1,1 +1,15 @@
-<h1>Opps..</h1>
+<?php get_header() ?>
+<style>
+    #main {
+        height: 60vh;
+    }
+</style>
+<div class="d-flex justify-content-center align-items-center" id="main">
+    <h1 class="mr-3 pr-3 align-top border-right inline-block align-content-center">404</h1>
+    <div class="inline-block align-middle">
+        <h2 class="font-weight-normal lead" id="desc">Sayfa bulunamadı.</h2>
+
+    </div>
+</div>
+
+<?php get_footer() ?>
