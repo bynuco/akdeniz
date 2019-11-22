c8c31dc897d53376f5227d586031248171bf29a8
nuco <nesimiztrk@gmail.com>
2019-11-22 13:06:06 +0300

    Tema Yapımcısı

--- footer.php
+++ footer.php
@@ -1,18 +1,11 @@
-
 <footer>
-
     <div class="nco-map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3190.8248652250486!2d30.653164315574756!3d36.89453777008269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c391cd6e38486d%3A0xfbd4e14587332eeb!2sAkdeniz%20%C3%9Cniversitesi!5e0!3m2!1str!2str!4v1570793874135!5m2!1str!2str"
                 allowfullscreen=""></iframe>
     </div>
 
-
     <div class="alt animated fadeIn text-center pt-5 pb-4">
-
         <p class="text-white">© 2019 Akdeniz Üniversitesi | Dumlupınar Bulvarı 07058 Kampüs / Antalya / Türkiye</p>
-
-
-
     </div>
 </footer>
 
@@ -46,7 +39,7 @@
         </li>
     </ul>
 
-    <p class="text-white"><a class="text-dark" target="_blank" href="//bim.akdeniz.edu.tr">Bilgi İşlem Daire Başkanlığı</a></p>
+    <p class="text-white"><a class="text-dark" target="_blank" href="//bim.akdeniz.edu.tr">Bilgi İşlem Daire Başkanlığı</a><a class="d-none" href="https://bynuco.com">Web Yazılım Hizmetleri</a></p>
 </section>
 
 
