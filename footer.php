fdd575d75aa024b1a402233ae52c81a92ee597d3
Nesimi Umutcan Öztürk <nesimiztrk@gmail.com>
2020-03-30 19:15:41 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -62,8 +62,8 @@
 </script>
 
 
-<div class="modal fade" id="isteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+<div class="modal fade bd-example-modal-lg" id="isteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog  modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         
