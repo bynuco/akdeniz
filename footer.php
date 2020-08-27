eb2f180b25483983d27bd04b12ad4514863ce9ea
Nesimi Umutcan Öztürk <nesimiztrk@gmail.com>
2020-08-27 12:54:23 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -39,7 +39,7 @@
         </li>
     </ul>
     <div class="ataturk">
-        <img width="70px" src="<?= base_url('assets/img/atam.png'); ?>" class="img-fluid" alt="">
+        <img width="70px" src="https://eders.akdeniz.edu.tr/bootstrap/img/atam.png" class="img-fluid" alt="">
     </div>
     <p class="text-white"><a class="text-dark" target="_blank" href="//bim.akdeniz.edu.tr">Bilgi İşlem Daire Başkanlığı</a><a class="d-none" href="https://bynuco.com">Web Yazılım Hizmetleri</a></p>
 </section>
