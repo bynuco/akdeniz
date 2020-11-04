fdc6287560d738543a593e44b0dc6cb648612da3
Nesimi Umutcan Öztürk <nesimiztrk@gmail.com>
2020-11-04 13:06:22 +0300

    Update page.php

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
 		<?php while ( have_posts() ) : the_post(); ?>
             <div class="container">
                 <div class="nco-single">
-                    <h1 class="mb-4">
+                    <h1 id="main" class="mb-4">
                         <i class="fas fa-angle-double-right"></i> <?= get_the_title(); ?>
                     </h1>
                     <div class="nco-content"><?php the_content(); ?></div>
