3d89f9271424a234cf5b9824962b7e7ccf92821c
Nesimi Umutcan Öztürk <nesimiztrk@gmail.com>
2021-01-22 14:47:49 +0300

    Update newsArchivePartial.php

--- partials/newsArchivePartial.php
+++ partials/newsArchivePartial.php
@@ -196,8 +196,8 @@
                 <div class="row">
 			<div class="col-lg-5">
 			<a href="https://eua.eu/" target="_blank">
-                            <img src="https://www.akdeniz.edu.tr/vendor/themes/akdeniz/assets/img/screenshot-eua.eu-2021.01.18-12_50_09.png" class="img-fluid img-thumbnail"
-                                 alt="Basında Bügün">
+                            <img src="http://www.bim.edu.tr/vendor/themes/akdeniz/assets/img/screenshot-eua.eu-2021.01.18-12_50_09.png" class="img-fluid img-thumbnail"
+                                 alt="EUA">
                         </a>
 			</div>
 			<div class="col-lg-2"></div>
