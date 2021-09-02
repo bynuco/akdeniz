e2932261d2ad1d8db2718d275809cb8d9d1b29c9
Nesimi Umutcan Öztürk <nesimiztrk@gmail.com>
2021-09-02 16:20:08 +0300

    Update sliderPartial.php

--- partials/sliderPartial.php
+++ partials/sliderPartial.php
@@ -51,3 +51,32 @@
         </div>
     </div>
 </section>
+
+
+<div class="modal fade bd-example-modal-lg" id="isteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog  modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <img src=""  class="img-fluid"/>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--
+<button id="isteModalButton" type="button" style="display:none" data-toggle="modal" data-target="#isteModal">
+</button>
+-->
+<script>/*
+    $( document ).ready(function() {
+        $("#isteModalButton").trigger('click');
+    });*/
+</script>
